4cfbaab59d59c924caae36d233e3a9eafdb36130
martti <info@martti.be>
2015-01-19 22:00:16 +0100

    license GPL-2.0

--- language/ar/common.php
+++ language/ar/common.php
@@ -3,7 +3,7 @@
 /**
 *  phpBB Extension - marttiphpbb templateevents
 * @copyright (c) 2014 marttiphpbb <info@martti.be>
-* @license http://opensource.org/licenses/MIT
+* @license GNU General Public License, version 2 (GPL-2.0)
 *
 * Translated By : Basil Taha Alhitary - www.alhitary.net
 */
