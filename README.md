4cad6036ed6227758b82e49cda0e2ade851caa74
martti <info@martti.be>
2018-03-30 18:57:26 +0200

    add twig extension on enable

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PhpBB Extension - marttiphpbb Template Events
 
-This extension for developers shows all core template events. Since version 1.3.0 also the core PHP events that were triggered for the current request are shown at the bottom of the page. 
+This extension for developers shows all core template events. Since version 1.3.0 also the core PHP events that were triggered for the current request are shown at the bottom of the page.
 
 **This extension is a helper tool for developing other extenstions. It should not be used on a live forum.**
 
@@ -10,12 +10,6 @@ This extension for developers shows all core template events. Since version 1.3.
 
 You can install this on the latest release of phpBB 3.2 by following the steps below:
 
-Do you have the "Recompile stale style components" option enabled?
-
-ACP > General > Server Configuration > Load Settings > Recompile stale style components
-
-
-* Before enabling this extension, make sure the option "Enable stale style components" in the ACP is enabled. See ACP > General > Server Configuration > Load Settings > Recompile stale style components.
 * Create `marttiphpbb/templateevents` in the `ext` directory.
 * Download and unpack the repository into `ext/marttiphpbb/templateevents`
 * Enable `Template Events` in the ACP at `Customise -> Manage extensions`.
--- ext.php
+++ ext.php
@@ -1,6 +1,6 @@
 <?php
 /**
-*  phpBB Extension - marttiphpbb templateevents
+* phpBB Extension - marttiphpbb templateevents
 * @copyright (c) 2014 - 2018 marttiphpbb <info@martti.be>
 * @license GNU General Public License, version 2 (GPL-2.0)
 */
@@ -8,8 +8,42 @@
 namespace marttiphpbb\templateevents;
 
 use phpbb\extension\base;
+use marttiphpbb\templateevents\twig\extension;
+use marttiphpbb\templateevents\service\events_cache;
+use marttiphpbb\templateevents\event\php_event_listener;
 
 class ext extends base
 {
+	/**
+	* @param mixed $old_state State returned by previous call of this method
+	* @return mixed Returns false after last step, otherwise temporary state
+	* @access public
+	*/
+	public function enable_step($old_state)
+	{
+		switch ($old_state)
+		{
+            case '':
+                // If phpBB was following a Post-Redirect-Get pattern then this was not necessary.
+                // To circumvent phpBB not redirecting after enabling the extension
+                // and failing to add the new Twig Extension it's add here in order to prevent
+                // a blank page on first response.
+                $twig = $this->container->get('template.twig.environment');
+                $request = $this->container->get('request');
+                $user = $this->container->get('user');
+                $language = $this->container->get('language');
+                $cache = $this->container->get('cache.driver');
+                $php_event_listener = new php_event_listener();
+                $events_cache = new events_cache($cache);
+                $twig_extension = new extension($request, $user, $language, $php_event_listener, $events_cache);
+                $twig->addExtension($twig_extension);
+                $language->add_lang('common', 'marttiphpbb/templateevents');
+				return 'add_twig_extension';
+			break;
+			default:
+				return parent::enable_step($old_state);
+			break;
+		}
+	}
 
 }
