5e97bb86f4890f0786059ce3f85fc5adf2b9728f
martti <info@martti.be>
2018-04-04 20:01:06 +0200

    template variables

--- event/listener.php
+++ event/listener.php
@@ -10,23 +10,41 @@ namespace marttiphpbb\templateevents\event;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use phpbb\event\data as event;
 use phpbb\request\request;
+use phpbb\user;
+use marttiphpbb\templateevents\service\events_cache;
+use marttiphpbb\templateevents\event\php_event_listener;
 
 /**
 * Event listener
 */
 class listener implements EventSubscriberInterface
 {
-	/* @var request */
-	protected $request;
+	/** @var request */
+	private $request;
+
+	/** @var user */
+	private $user;
+
+	/** @var events_cache */
+	private $events_cache;
+
+	/** @var php_event_listener */
+	private $php_event_listener;
 
 	/**
 	 * @param request $request
 	*/
 	public function __construct(
-		request $request
+		request $request,
+		user $user,
+		events_cache $events_cache,
+		php_event_listener $php_event_listener
 	)
 	{
 		$this->request = $request;
+		$this->user = $user;
+		$this->events_cache = $events_cache;
+		$this->php_event_listener = $php_event_listener;
 	}
 
 	static public function getSubscribedEvents()
@@ -34,6 +52,8 @@ class listener implements EventSubscriberInterface
 		return [
 			'core.user_setup'		=> 'core_user_setup',
 			'core.append_sid'		=> 'core_append_sid',
+			'core.twig_environment_render_template_before'
+				=> ['core_twig_environment_render_template_before', -1],
 		];
 	}
 
@@ -85,4 +105,36 @@ class listener implements EventSubscriberInterface
 			$event['params'] = $params;
 		}
 	}
+
+	public function core_twig_environment_render_template_before(event $event)
+	{
+		$context = $event['context'];
+
+		$page_name = $this->user->page['page_name'];
+		$query_string = $this->user->page['query_string'];	
+		$query_string = str_replace(['&templateevents=1', '&templateevents=0'], '', $query_string);
+		$query_string = str_replace(['templateevents=1', 'templateevents=0'], '', $query_string);
+		$query_string = trim($query_string, '&');
+		$query_string .= $query_string ? '&' : '';
+
+		$php_count_ary = $this->php_event_listener->get_count_ary();
+		$php_events = [];
+		$events = $this->events_cache->get_all();
+
+		foreach ($php_count_ary as $name => $count)
+		{
+			$php_events[$name] = $events['php'][$name];
+			$php_events[$name]['count'] = $count;
+		}
+
+		$template = [
+			'enable'	=> $this->request->variable('templateevents', 0) ? true : false,
+			'u_hide'	=> append_sid($page_name, $query_string . 'templateevents=0'),
+			'u_show'	=> append_sid($page_name, $query_string . 'templateevents=1'),
+			'php'		=> $php_events,
+		];
+
+		$context['marttiphpbb_templateevents'] = $template;
+		$event['context'] = $context;		
+	}	
 }
