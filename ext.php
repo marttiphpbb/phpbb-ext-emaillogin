8e0276ae9098003237e18b79b0f69317cc85baa9
martti <info@martti.be>
2018-05-02 06:53:43 +0200

    phpBB & PHP requirements

--- ext.php
+++ ext.php
@@ -11,4 +11,13 @@ use phpbb\extension\base;
 
 class ext extends base
 {
+	/**
+	 * phpBB 3.2.x and PHP 7+
+	 */
+	public function is_enableable()
+	{
+		$config = $this->container->get('config');
+		return phpbb_version_compare($config['version'], '3.2', '>=') && version_compare(PHP_VERSION, '7', '>=');
+	}
+
 }
