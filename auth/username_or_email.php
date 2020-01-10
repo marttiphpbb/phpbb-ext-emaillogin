9c70ca2aa81e8059d51e4a9064f7eff3d21e1d74
martti <info@martti.be>
2020-01-10 07:11:39 +0100

    copyright 2020

--- auth/username_or_email.php
+++ auth/username_or_email.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * phpBB Extension - marttiphpbb emaillogin
-* @copyright (c) 2018 - 2019 marttiphpbb <info@martti.be>
+* @copyright (c) 2018 - 2020 marttiphpbb <info@martti.be>
 * @license GNU General Public License, version 2 (GPL-2.0)
 */
 
